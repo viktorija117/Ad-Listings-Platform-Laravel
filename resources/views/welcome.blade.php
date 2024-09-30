1cff00431dde2831c4be657dc1f67a615528462c
viktorija117 <viktorija.kitanovic.17@gmail.com>
2024-09-30 18:58:43 +0200

    Stilizacija

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dobrodošli na KupujemProdajem Repliku</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body class="bg-gray-100">
 
 <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
     <div class="max-w-lg w-full mx-auto text-center">
-        <h1 class="text-4xl font-bold text-gray-900 mb-6">Dobrodošli na KupujemProdajem Repliku</h1>
+        <h1 class="text-4xl font-bold text-gray-900 mb-6">Dobrodošli na KupujemProdajem!</h1>
         <p class="text-gray-700 text-lg mb-8">
             Postavite oglase, pronađite najbolje ponude i povežite se sa drugim korisnicima.
         </p>
